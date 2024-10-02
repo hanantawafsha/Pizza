1a59a55b3b3afca01c8c4ba8d17bffe499a1d696
hanantawafsha <hananjtawafsha@gmail.com>
2024-10-02 15:19:05 +0300

    upload files

---
+++ assets/main.js
@@ -0,0 +1,26 @@
+async function getPizza() {
+const response = await fetch('https://forkify-api.herokuapp.com/api/search?q=pizza');
+const data = await response.json();
+console.log(data);
+
+const pizza = data.recipes;
+
+//console.log(pizza.source_url);
+
+const result = pizza.map(function(pizzaitem){
+return `<div class="pizzaitem">
+    <h2>${pizzaitem.title}</h2>
+    <img src="${pizzaitem.image_url}" alt="${pizzaitem.title}">
+</div>`;
+
+}).join('');
+
+document.querySelector(".container .row").innerHTML=result;
+
+}
+
+// "title": "Deep Dish Fruit Pizza",
+//"image_url": "http://thepioneerwoman.com/cooking/2012/01/fruit-pizza/",
+
+
+getPizza();
